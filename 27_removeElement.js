79bf4b18f478aaf7ca0b395291a20c1eb41bfb2c
AartiGhorpade <asghorpade99@gmail.com>
2026-05-14 21:48:46 +0530

    remove elem

--- 27_removeElement.js
+++ 27_removeElement.js
@@ -1,11 +1,15 @@
 var removeElement = function (nums, val) {
+    let start = 0
+
     for (let i = 0; i < nums.length; i++) {
         if (nums[i] == val) {
-            nums.pop(nums[i])   
+            nums[start] = nums[i]
+            start++
         }
     }
-
-
+    return nums.slice(0, start)
 };
 
-console.log(removeElement([3,2,2,3]));
+console.log(removeElement([3, 2, 2, 3], 3))
+
+module.exports = removeElement;
